3e166ac8715c47c16bc39ee58533e338621bcb0b
omoniyi <niyibolarinde63@gmail.com>
2025-05-12 19:52:36 +0100

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,1 +1,13 @@
+document.getElementById('signupForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const username = document.getElementById('signupUsername').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('signupPassword').value;
+
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+  users.push({ username, email, password });
+  localStorage.setItem('users', JSON.stringify(users));
+
+  window.location.href = 'login.html';
+});
 
